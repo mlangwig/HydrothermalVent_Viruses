600abb46d6e3075966f2119c1239a197f9348d41
Marguerite Langwig <marguerite.langwig@gmail.com>
2023-11-01 14:51:35 -0500

    create abun plot Gamma Camp

--- VentVirus_Analysis/Abundance.R
+++ VentVirus_Analysis/Abundance.R
@@ -78,7 +78,7 @@ abun_long_iphop_p <- abun_long_iphop %>%
   group_by(Site, Taxa) %>% 
   summarise(value=sum(as.numeric(value))) %>% #summing the group
   filter(grepl("c__Gammaproteobacteria|p__Campylobacterota", Taxa)) %>% #only grab Gamma and Campylo
-  ungroup() 
+  ungroup()
 
 ############################ Set order of sites for plotting #############################
 
@@ -99,22 +99,26 @@ abun_long_iphop_p <- abun_long_iphop %>%
 # #abun_long_iphop_p$value <- factor(abun_long_iphop_p$value, levels = abun_long_iphop_p$value)
 # abun_long_iphop_p$Site <- factor(abun_long_iphop_p$Site, levels = unique(abun_long_iphop_p$Site))
 
+abun_long_iphop_p$Site <- gsub("min1000","Plume", abun_long_iphop_p$Site) 
+abun_long_iphop_p$Site <- gsub("_scaffolds_Plume","", abun_long_iphop_p$Site) 
+abun_long_iphop_p$Site <- gsub("Seawater_scaffolds_Plume","Seawater", abun_long_iphop_p$Site) 
+
 #set order of x axis 
 abun_long_iphop_p$Site <- factor(abun_long_iphop_p$Site, 
-                                 levels=c("Lau_Basin_Kilo_Moana_min1000_4", "Lau_Basin_Tahi_Moana_min1000_2", 
+                                 levels=c("Lau_Basin_Kilo_Moana_Plume_4", "Lau_Basin_Tahi_Moana_Plume_2", 
                                           "Brothers_NWCB_S139", "Brothers_NWCB_S012",
-                                          "Lau_Basin_Abe_min1000_2", "Lau_Basin_Abe_min1000_3", 
-                                          "Cayman_Shallow_min1000_2", "MAR_Rainbow_354-166",
-                                          "Brothers_NWCB_S140", "MAR_Rainbow_355-202", "Cayman_Deep_min1000_3", 
-                                          "Brothers_NWCB_S141", "EPR_4281-140", "Lau_Basin_Kilo_Moana_min1000_2", 
-                                          "Cayman_Shallow_min1000_1", "Lau_Basin_Tahi_Moana_min1000_1",
-                                          "Brothers_NWCB_S146", "Cayman_Deep_min1000_2", "Lau_Basin_Kilo_Moana_min1000_3",
-                                          "ELSC_Tui_Malila_134-614", "ELSC_Tui_Malila_T2", "Lau_Basin_Mariner_min1000_2",
-                                          "Lau_Basin_Mariner_min1000_1", "Lau_Basin_Kilo_Moana_min1000_1",
-                                          "Lau_Basin_Abe_min1000_1", "Cayman_Shallow_min1000_3", "MAR_Lucky_356-308", "ELSC_Abe_128-326",
-                                          "ELSC_Abe_A3", "ELSC_Vai_Lili_V2", "MAR_Lucky_356-284", "Lau_Basin_Tui_Malila_min1000",
-                                          "Cayman_Deep_min1000_1", "Axial_Plume_scaffolds_min1000", "Guaymas_Basin_min1000",
-                                          "Brothers_Diffuse_S009", "Axial_Seawater_scaffolds_min1000", "EPR_PIR-30", #end Gamma highest to lowest
+                                          "Lau_Basin_Abe_Plume_2", "Lau_Basin_Abe_Plume_3", 
+                                          "Cayman_Shallow_Plume_2", "MAR_Rainbow_354-166",
+                                          "Brothers_NWCB_S140", "MAR_Rainbow_355-202", "Cayman_Deep_Plume_3", 
+                                          "Brothers_NWCB_S141", "EPR_4281-140", "Lau_Basin_Kilo_Moana_Plume_2", 
+                                          "Cayman_Shallow_Plume_1", "Lau_Basin_Tahi_Moana_Plume_1",
+                                          "Brothers_NWCB_S146", "Cayman_Deep_Plume_2", "Lau_Basin_Kilo_Moana_Plume_3",
+                                          "ELSC_Tui_Malila_134-614", "ELSC_Tui_Malila_T2", "Lau_Basin_Mariner_Plume_2",
+                                          "Lau_Basin_Mariner_Plume_1", "Lau_Basin_Kilo_Moana_Plume_1",
+                                          "Lau_Basin_Abe_Plume_1", "Cayman_Shallow_Plume_3", "MAR_Lucky_356-308", "ELSC_Abe_128-326",
+                                          "ELSC_Abe_A3", "ELSC_Vai_Lili_V2", "MAR_Lucky_356-284", "Lau_Basin_Tui_Malila_Plume",
+                                          "Cayman_Deep_Plume_1", "Axial_Plume", "Guaymas_Basin_Plume",
+                                          "Brothers_Diffuse_S009", "Axial_Seawater", "EPR_PIR-30", #end Gamma highest to lowest
                                           "Brothers_Diffuse_S015", "ELSC_Abe_A1", "Brothers_NWCA_S143", "Brothers_LC_S014",
                                           "ELSC_Tui_Malila_T11", "ELSC_Tui_Malila_T10", "Brothers_NWCA_S144",
                                           "Guaymas_4571-419", "Guaymas_4559-240", "Brothers_LC_S016",
@@ -123,6 +127,8 @@ abun_long_iphop_p$Site <- factor(abun_long_iphop_p$Site,
                                           "Brothers_UC_S147", "ELSC_Bowl_M1", "Brothers_UC_S010", "Brothers_NWCA_S017",
                                           "Brothers_UC_S011", "ELSC_Mariner_M10", "Guaymas_4561-384",
                                           "Brothers_NWCA_S145", "Brothers_NWCA_S142")) # start Camp lowest to highest 
+
+
 ############################ Plot  #############################
 
 
@@ -133,7 +139,7 @@ plot <- abun_long_iphop_p %>%
   geom_bar(stat = "identity") +
   scale_fill_viridis_d(begin = .5,
                        end = 0) +
-  labs(x = "% Relative Abundance", y = "Site") +
+  labs(x = "Percent Relative Abundance", y = "Site") +
   guides(fill=guide_legend(override.aes = list(size=3))) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5),
@@ -145,14 +151,14 @@ plot <- abun_long_iphop_p %>%
         plot.background = element_rect(fill = "transparent", color = NA)) +
         #panel.border = element_blank()) + #turn this off to get the outline back)
   scale_x_continuous(expand = c(0, 0)) + #turn this on to make it look aligned with ticks
-  ggtitle("Viral Abundance") #+ #Change for top X grabbed
+  ggtitle("Viral Abundance") + #Change for top X grabbed
   #facet_wrap(.~Taxa) 
-#scale_y_continuous(breaks = seq(0, 45, by=5))
+  scale_y_discrete(limits=rev)
 #coord_flip()
 plot
 
-# ggsave("Output/coverm_abun.png", plot, width = 10, height = 5, dpi = 500,
-#        bg = "transparent")
+ggsave("Output/coverm_abun_CampGamma.png", plot, width = 10, height = 8, dpi = 500,
+       bg = "transparent")
 # ggsave("Output/coverm_abun.pdf", plot, width = 10, height = 5, dpi = 500,
 #        bg = "transparent")
 
