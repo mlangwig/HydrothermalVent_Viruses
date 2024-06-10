12a9c0463b70a156437c2a80a7a9fc5e0951f7a5
Marguerite Langwig <marguerite.langwig@gmail.com>
2024-06-10 16:32:13 -0500

    permanova phyloseq

--- VentVirus_Analysis/Diversity.R
+++ VentVirus_Analysis/Diversity.R
@@ -33,10 +33,15 @@ abund_norm_wide <- abund_norm_wide %>%
 abund_norm_wide <- as.data.table(abund_norm_wide)
 #create the "OTU matrix" aka viral genome relative abundances matrix  
 vmagmat <- as.matrix(abund_norm_wide, rownames = "Genome")
+
+#make sure the column order is alphabetical
+vmagmat <- vmagmat[sort(rownames(vmagmat)), sort(colnames(vmagmat))]
+
 # write.table(vmagmat, file = "output/vmag_mat.tsv", quote = FALSE,
 #             row.names = FALSE, sep = "\t")
 
 ######## taxonomy metadata matrix
+
 missing_genomes <- abund_norm_wide$Genome[!(abund_norm_wide$Genome %in% genomad_tax$genome)]
 new_rows <- data.frame(genome = missing_genomes,
                        r = "Unknown",
@@ -62,9 +67,14 @@ genomad_tax2 <- genomad_tax2 %>%
 genomad_tax2 <- as.data.table(genomad_tax2)
 taxmat <- as.matrix(genomad_tax2, rownames = "genome")
 
+#make sure the column order is alphabetical
+taxmat <- taxmat[sort(rownames(taxmat)),]
+
 ######## sample metadata matrix
 
 sampledata <- read.delim(file = "input/sample_metadata_div.txt", header = TRUE, row.names = "Site")
+#make sure the column order is alphabetical
+sampledata <- sampledata[sort(rownames(sampledata)),]
 
 # #convert to data_table and then matrix
 # sampledata <- as.data.table(sampledata, rownames = "Site")
@@ -74,7 +84,6 @@ sampledata <- read.delim(file = "input/sample_metadata_div.txt", header = TRUE, 
 
 #loading phyloseq object code taken from https://joey711.github.io/phyloseq/import-data
 
-library("phyloseq")
 #create OTU and TAX objects
 OTU = otu_table(vmagmat, taxa_are_rows = TRUE)
 TAX = tax_table(taxmat)
@@ -82,6 +91,12 @@ SAMP = sample_data(sampledata)
 
 #create phyloseq object
 physeq = phyloseq(OTU, TAX, SAMP)
+
+#check
+otu_table(physeq)[1:5, 1:5]
+ntaxa(physeq)
+nsamples(physeq)
+sample_variables(physeq)
 
 # rest based on this guide: https://rpubs.com/lgschaerer/betadiv
 
@@ -95,13 +110,39 @@ bray <- ordinate(
 
 head(sample_data(physeq))
 
+###################################### phyloseq plot NMDS ordination ######################################################
+
 dev.off()
 plot_ordination(
   physeq = physeq,                                                          
-  ordination = bray,
-  label = "Hydrothermal_Vent") +
-  geom_point(aes(color = General_Site, shape = Type), size = 6)
-
+  ordination = bray) + #,label = "Hydrothermal_Vent"
+  geom_point(aes(color = General_Site, shape = Type), size = 3) + #shape size on NMDS
+  scale_color_manual(values=c("#4F508C","#B56478","#CE9A28","#28827A", "#3F78C1",
+                            "#8c510a", "#000000")) +
+  theme_linedraw() +
+  geom_text(mapping = aes(label = Hydrothermal_Vent, hjust = 1, vjust = 1.5), size = 3) +
+  theme(                             
+    legend.title = element_blank(),                                          #removes legend title
+    #legend.position = "bottom",
+    legend.text = element_text(size = 15, face = "bold"),                                 
+    axis.text.y.left = element_text(size = 10),
+    axis.title.y = element_text(size = 15),
+    axis.text.x = element_text(size = 10),
+    axis.title.x = element_text(size = 15),
+    strip.text = element_text(face = "bold", size = 15)) +
+  guides(fill = guide_legend(override.aes = list(shape = 21))) #+          #fills legend points based on the fill command
+  #facet_wrap(~Type)  
+  
 #have to separate plume and vent to see things
+
+
+############################################ phyloseq PERMANOVA ######################################################
+
+#Calculate distance and save as a matrix
+bray <- phyloseq::distance(physeq, method = "bray")
+#sam <- data.frame(sample_data(physeq))
+#Run PERMANOVA on distances.
+adonis2(bray ~ General_Site*Type*Latitude_DD*Longitude_DD*Depth_m, data = sampledata, permutations = 1000)
+
 
 
